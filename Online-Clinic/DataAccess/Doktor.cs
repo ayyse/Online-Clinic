7286fbdd8fcb5ec5d98b29c3cc56e227ea9b177d
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2020-11-28 15:01:00 +0300

    Updated

---
+++ Online-Clinic/DataAccess/Doktor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Clinic.DataAccess
+{
+    [Table("Doktorlar")]
+    public class Doktor
+    {
+        [Column("DoktorID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int DoktorID { get; set; }
+
+        [Column("Doktor Adı Soyadı")]
+        [Required]
+        [StringLength(100)]
+        public string Ad { get; set; }
+
+        [ForeignKey("DoktorID")]
+        public virtual List<Hasta> Hastalar { get; set; }
+    }
+}
---
+++ Online-Clinic/DataAccess/Hasta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Clinic.DataAccess
+{
+    [Table("Hastalar")]
+    public class Hasta
+    {
+        [Column("HastaID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int HastaID { get; set; }
+
+        [Column("Hasta Adı Soyadı")]
+        [Required]
+        [StringLength(100)]
+        public string Ad { get; set; }
+
+        [ForeignKey("HastaID")]
+        public virtual List<Doktor> Doktorlar { get; set; }
+    }
+}
