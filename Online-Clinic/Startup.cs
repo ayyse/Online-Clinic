759d205d8ac9eea5020466e02217a614e70d9903
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2020-12-01 12:03:53 +0300

    Updated

--- Online-Clinic/Startup.cs
+++ Online-Clinic/Startup.cs
@@ -42,10 +42,16 @@ namespace Online_Clinic
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //app.UseMvc(routes => {
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{Controller=Home}/{action=Index}");
+            //});
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //The default HSTS value is 30 days.You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
