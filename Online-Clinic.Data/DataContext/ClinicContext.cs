4e52acffe6e79c955e7894ae61b0773d467127f0
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2020-12-09 21:13:43 +0300

    Updated
    Layered architecture
    Identity

---
+++ Online-Clinic.Data/DataContext/ClinicContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Clinic.Data.DataContext
+{
+    public class ClinicContext : IdentityDbContext
+    {
+        public ClinicContext(DbContextOptions<ClinicContext> options)
+            : base(options)
+        {
+            
+        }
+
+    }
+}
--- Online-Clinic/Startup.cs
+++ Online-Clinic/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Online_Clinic.DataAccess;
+using Online_Clinic.Data.DataContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,14 +26,15 @@ namespace Online_Clinic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
+            services.AddDbContext<ClinicContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
             services.AddControllersWithViews();
             services.AddDbContext<AppDbContext>
-           (o => o.UseSqlServer(Configuration.
-            GetConnectionString("MyNewDatabase")));
+                (o => o.UseSqlServer(Configuration.GetConnectionString("MyNewDatabase")));
         }
 
-        /*public void ConfigureServices(IServiceCollection services)
-        => services.AddDbContext<AppDbContext>();*/
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,9 +71,9 @@ namespace Online_Clinic
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                //endpoints.MapControllerRoute(
-                //   name: "MyArea",
-                //   pattern: "{area:Admin}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                   name: "MyArea",
+                   pattern: "{area:Admin}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
